e2248e87a833dac8bf64a8616f8c52c0f366dbc1
Gao1453979869 <51064453+Gao1453979869@users.noreply.github.com>
2019-05-28 14:23:05 +0800

    Update App.java

--- src/com/qianfeng/haha/App.java
+++ src/com/qianfeng/haha/App.java
@@ -3,7 +3,7 @@ package com.qianfeng.haha;
 public class App {
     public static void main(String[] args) {
         System.out.println("haha");
-        System.out.println("dasd");
+       // System.out.println("dasd");
          System.out.println("aaaaa");
     }
 }
